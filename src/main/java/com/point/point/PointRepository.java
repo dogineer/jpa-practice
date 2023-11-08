6180847dab9211949ef98ba0c31c2531d02273c4
dogineer <dogineer.dev@gmail.com>
2023-11-08 17:51:56 +0900

    add: findPointsByUserId 추가

--- src/main/java/com/point/point/PointRepository.java
+++ src/main/java/com/point/point/PointRepository.java
@@ -2,9 +2,12 @@ package com.point.point;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PointRepository extends JpaRepository<Point, UUID> {
     Point findPointById(UUID id);
     Point findPointByUserId(UUID userId);
+
+    List<Point> findPointsByUserId(UUID userId);
 }
