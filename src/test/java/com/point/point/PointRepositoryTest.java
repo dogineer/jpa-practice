f7f8c6bfe50de0375a9244560470fd467e6ce410
dogineer <dogineer.dev@gmail.com>
2023-11-09 15:20:56 +0900

    test: 유저 포인트 사용 테스트

--- src/test/java/com/point/point/PointRepositoryTest.java
+++ src/test/java/com/point/point/PointRepositoryTest.java
@@ -99,4 +99,65 @@ public class PointRepositoryTest {
             .usingRecursiveComparison()
             .isEqualTo(userPoints);
     }
+
+    @Test
+    @DisplayName("유저 포인트 사용")
+    @Transactional
+    public void useUserPoint() {
+        UUID userId = testUser();
+
+        List<Point> addPoints = new ArrayList<>(IntStream.range(0, 5)
+            .mapToObj(i -> Point.builder()
+                .id(UUID.randomUUID())
+                .userId(userId)
+                .point(1000)
+                .date(date)
+                .build())
+            .toList());
+
+        pointRepository.saveAll(addPoints);
+
+        List<Point> userPoints = pointRepository.findPointsByUserId(userId);
+        userPoints.sort(Comparator.comparing(Point::getDate));
+
+        List<Point> totalPoints = pointRepository.findPointsByUserId(userId);
+
+        int userTotalPoint = totalPoints.stream()
+            .mapToInt(Point::getPoint)
+            .sum();
+
+        int usePoint = 3000;
+
+        System.out.println("현재 가지고 있는 포인트 : " + userTotalPoint);
+        System.out.println("사용할 포인트 : " + usePoint);
+
+        if (usePoint <= userTotalPoint) {
+            List<Point> updatedPoints = new ArrayList<>();
+
+            for (Point point : totalPoints) {
+                int pointValue = point.getPoint();
+
+                if (usePoint >= pointValue) {
+                    usePoint -= pointValue;
+                } else {
+                    updatedPoints.add(Point.builder()
+                        .id(point.getId())
+                        .userId(point.getUserId())
+                        .point(pointValue - usePoint)
+                        .date(point.getDate())
+                        .build());
+                    usePoint = 0;
+                }
+            }
+
+            totalPoints = updatedPoints;
+
+            System.out.println(totalPoints);
+            System.out.println("남은 포인트 : " + totalPoints.stream()
+                .mapToInt(Point::getPoint)
+                .sum());
+        } else {
+            throw new RuntimeException("현재 유저가 갖고 있는 포인트 보다 사용할 포인트가 더 큽니다.");
+        }
+    }
 }
